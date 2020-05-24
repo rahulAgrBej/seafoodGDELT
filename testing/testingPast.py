a01e3fd6d97a23aca490440ed61eb0d05f9e67da
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-05-24 16:06:17 -0400

    request made for 2017-2018 articles with query - seafood outbreak

--- testing/testingPast.py
+++ testing/testingPast.py
@@ -5,9 +5,11 @@ import pprint
 gdeltAPI = 'https://api.gdeltproject.org/api/v2/doc/doc'
 
 payload = {}
-payload['QUERY'] = 'seafood "COVID-19" thai'
+payload['QUERY'] = 'seafood outbreak'
 payload['MODE'] = 'ArtList'
 payload['FORMAT'] = 'JSON'
+payload['STARTDATETIME'] = '20170101000000'
+payload['ENDDATETIME'] = '20180101000000'
 
 # testing GDELT DOC API request for November 2019
 resp = requests.get(gdeltAPI, params=payload)
