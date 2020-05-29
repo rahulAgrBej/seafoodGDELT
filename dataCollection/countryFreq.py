d1849ab9746e7c17036d02bd2c5bf8248fa76720
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-05-28 22:52:34 -0400

    collectin num articles per month with source country example Ghana

--- dataCollection/countryFreq.py
+++ dataCollection/countryFreq.py
@@ -2,11 +2,13 @@ import pycountry
 import requests
 import json
 import pprint
+import matplotlib.pyplot as plt
+from changeDateParams import incrementMonth, incrementYear
 
 gdeltAPI = 'https://api.gdeltproject.org/api/v2/doc/doc'
 
 payload = {}
-payload['QUERY'] = 'seafood "COVID-19"'
+payload['QUERY'] = 'seafood "COVID-19" sourcecountry:GH'
 payload['MODE'] = 'ArtList'
 payload['FORMAT'] = 'JSON'
 
@@ -15,8 +17,8 @@ payload['MAXRECORDS'] = '250'
 
 # Will just check the month of November 2019
 # date format YYYYMMDDHHMMSS
-dateStart = '20191101000000'
-dateEnd = '20191130235959'
+dateStart = '20200101000000'
+dateEnd = incrementMonth(dateStart, 1)
 
 # Will only search through articles posted through dateStart-dateEnd
 payload['STARTDATETIME'] = dateStart
@@ -26,10 +28,21 @@ payload['ENDDATETIME'] = dateEnd
 countriesList = list(pycountry.countries)
 
 # EXAMPLE: will only get articles from Spain
-payload['SourceCountry'] = 'Spain'
+payload['SOURCECOUNTRY'] = 'ghana'
 
-# testing GDELT DOC API request for 2019 - 2020
-resp = requests.get(gdeltAPI, params=payload)
-print(f'response code: {resp.status_code}')
-results = resp.json()
-print(results.keys())
+# make GDELT request for articles for spain mentioning seafood AND "COVID-19" during NOV 2019
+
+
+for i in range(4):
+    resp = requests.get(gdeltAPI, params=payload)
+    print(f'response code: {resp.status_code}')
+    results = resp.json()
+    if len(results.keys()) != 0:
+        print(len(results['articles']))
+    else:
+        print(0)
+    payload['STARTDATETIME'] = payload['ENDDATETIME']
+    payload['ENDDATETIME'] = incrementMonth(payload['ENDDATETIME'], 1)
+
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(results)
