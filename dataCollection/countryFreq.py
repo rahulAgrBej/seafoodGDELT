fd25ae12e51c475aadb16adcf9951e2faf857902
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-03 14:51:17 -0400

    works for for first 5 countries fetching for month results

--- dataCollection/countryFreq.py
+++ dataCollection/countryFreq.py
@@ -21,9 +21,22 @@ MONTH_END = {
     12: 31
 }
 
+COUNTRIES = []
+
+# reads in all GDELT supported Countries
+countryFile = open('supportedCountries.txt', 'r')
+numCountries = int(countryFile.readline().rstrip('\n'))
+for cCount in range(numCountries):
+    line = countryFile.readline().rstrip('\n')
+    countryData = line.split('\t')
+    COUNTRIES.append(countryData)
+countryFile.close()
+
 def collectMonthNums(inURL, inPayload):
 
     resp = requests.get(gdeltAPI, params=inPayload)
+    print(resp)
+    print(resp.content)
     results = resp.json()
 
     numArticles = 0
@@ -31,7 +44,7 @@ def collectMonthNums(inURL, inPayload):
     if len(results.keys()) != 0:
         
         # if we got the MAX number of article results
-        if results['articles'] >= MAX_ARTICLES:
+        if len(results['articles']) >= MAX_ARTICLES:
             
             # Do a more granular search (day by day search)
             startDay = inPayload['STARTDATETIME']
@@ -46,6 +59,8 @@ def collectMonthNums(inURL, inPayload):
                     numArticles += results['articles']
                 
                 startDay = endDay
+        else:
+            numArticles = len(results['articles'])
 
     return numArticles
         
@@ -69,31 +84,46 @@ payload['STARTDATETIME'] = dateStart
 payload['ENDDATETIME'] = dateEnd
 
 # Gets a list of all countries in the world
-# countriesList = list(pycountry.countries)
-countryList = ['GH', 'TH', 'NO', 'VM', 'CI']
+#countriesList = list(pycountry.countries)
+#countryList = ['GH', 'TH', 'NO', 'VM', 'CI']
 
 countryFreq = {}
+"""
 for country in countryList:
     countryFreq[country] = []
+"""
 
-# make GDELT request for articles for spain mentioning seafood AND "COVID-19" during NOV 2019
+# make GDELT requests for each country in our list of countries from Jan 2020 - May 2020
 for i in range(5):
-
-    for country in countryList:
-        payload['QUERY'] = 'seafood "COVID-19" sourcecountry:' + country
-        resp = requests.get(gdeltAPI, params=payload)
-        results = resp.json()
     
-        if len(results.keys()) != 0:
-            countryFreq[country].append(len(results['articles']))
-        else:
-            countryFreq[country].append(0)
+    for countryIdx in range(len(COUNTRIES))[:5]:
+        
+        countryCode = COUNTRIES[countryIdx][0]
+        countryName = COUNTRIES[countryIdx][1]
+        payload['QUERY'] = 'seafood "COVID-19" sourcecountry:' + countryCode
+        numArticles = collectMonthNums(gdeltAPI, payload)
+
+        # if its the first time we are getting data for this country
+        if not (countryName in countryFreq.keys()):
+            countryFreq[countryName] = {}
+            countryFreq[countryName]['seafood&COVID-19'] = []
+            countryFreq[countryName]['seafood'] = []
+        
+        countryFreq[countryName]['seafood&COVID-19'].append(numArticles)
+        
+        payload['QUERY'] = 'seafood sourcecountry:' + countryCode
+        numArticles = collectMonthNums(gdeltAPI, payload)
+        
+        countryFreq[countryName]['seafood'].append(numArticles)
     
     payload['STARTDATETIME'] = payload['ENDDATETIME']
     payload['ENDDATETIME'] = incrementMonth(payload['ENDDATETIME'], 1)
+    print(countryFreq)
+    print()
 
-
+"""
 monthNames = ['Jan', 'Feb', 'March', 'April', 'May']
+
 
 countryScatters = []
 for country in countryList:
@@ -102,3 +132,4 @@ for country in countryList:
 plt.legend()
 
 plt.savefig('dataCollection/sample.png')
+"""
