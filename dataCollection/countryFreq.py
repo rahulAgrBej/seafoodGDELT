caecb95cfd5ea281ac33f1680f9642ef648ddc75
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-03 16:14:35 -0400

    works from month to day to hour

--- dataCollection/countryFreq.py
+++ dataCollection/countryFreq.py
@@ -3,7 +3,7 @@ import requests
 import json
 import pprint
 import matplotlib.pyplot as plt
-from changeDateParams import getMonth, incrementDay, incrementMonth, incrementYear
+from changeDateParams import getMonth, incrementMin, incrementHour, incrementDay, incrementMonth, incrementYear
 
 MAX_ARTICLES = 250
 MONTH_END = {
@@ -32,37 +32,89 @@ for cCount in range(numCountries):
     COUNTRIES.append(countryData)
 countryFile.close()
 
+def collectMinutelyNums(inURL, inPayload):
+    minuteCount = 0
+
+    startMin = inPayload['STARTDATETIME']
+    for i in range(59):
+        endMin = incrementMin(startMin, 1)
+        inPayload['ENDDATETIME'] = endMin
+        minuteResp = requests.get(inURL, inPayload)
+        minuteResults = minuteResp.json()
+        if (len(minuteResults.keys()) != 0):
+            minuteArticles = len(minuteResults['articles'])
+            print(f'minute {i}: {minuteArticles}')
+            minuteCount += minuteArticles
+        
+        startMin = endMin
+        inPayload['STARTDATETIME'] = startMin
+    
+    return minuteCount
+
+def collectHourlyNums(inURL, inPayload):
+
+    hourCount = 0
+
+    startHour = inPayload['STARTDATETIME']
+    for i in range(23):
+        endHour = incrementHour(startHour, 1)
+        inPayload['ENDDATETIME'] = endHour
+        hourlyResp = requests.get(inURL, inPayload)
+        hourResults = hourlyResp.json()
+        if (len(hourResults.keys()) != 0):
+            hourArticles = len(hourResults['articles'])
+            if hourArticles >= MAX_ARTICLES:
+                minuteArticles = collectMinutelyNums(inURL, inPayload)
+                hourArticles = minuteArticles
+            print(f'hour {i}: {hourArticles}')
+            hourCount += hourArticles
+
+        startHour = endHour
+        inPayload['STARTDATETIME'] = startHour
+    
+    return hourCount
+
+def collectDailyNums(inURL, inPayload):
+
+    dailyCount = 0
+    
+    startDay = inPayload['STARTDATETIME']
+    month = int(getMonth(startDay))
+    for i in range (MONTH_END[month]):
+        endDay = incrementDay(startDay, 1)
+        inPayload['ENDDATETIME'] = endDay
+        dailyResp = requests.get(inURL, inPayload)
+        dailyResults = dailyResp.json()
+
+        if (len(dailyResults.keys()) != 0):
+            dailyArticles = len(dailyResults['articles'])
+            if dailyArticles >= MAX_ARTICLES:
+                hourArticles = collectHourlyNums(inURL, inPayload)
+                dailyArticles = hourArticles
+            print(f'day {i}: {dailyArticles}')
+            dailyCount += dailyArticles
+        
+        startDay = endDay
+        inPayload['STARTDATETIME'] = startDay
+    
+
+    return dailyCount
+
 def collectMonthNums(inURL, inPayload):
 
-    resp = requests.get(gdeltAPI, params=inPayload)
-    print(resp)
-    results = resp.json()
+    monthlyCount = 0
 
-    numArticles = 0
+    monthlyResp = requests.get(gdeltAPI, params=inPayload)
+    monthlyResults = monthlyResp.json()
+    if len(monthlyResults.keys()) != 0:
+        monthlyArticles = len(monthlyResults['articles'])
+        if monthlyArticles >= MAX_ARTICLES:
+            dailyArticles = collectDailyNums(inURL, inPayload)
+            monthlyArticles = dailyArticles
+        print(f'month : {monthlyArticles}')
+        monthlyCount += monthlyArticles
 
-    if len(results.keys()) != 0:
-        
-        # if we got the MAX number of article results
-        if len(results['articles']) >= MAX_ARTICLES:
-            
-            # Do a more granular search (day by day search)
-            startDay = inPayload['STARTDATETIME']
-            month = int(getMonth(startDay))
-            for i in range (MONTH_END[month]):
-                
-                endDay = incrementDay(startDay, 1)
-                inPayload['ENDDATETIME'] = endDay
-                dailyResp = requests.get(inURL, inPayload)
-                
-                if (len(results.keys()) != 0):
-                    numArticles += len(results['articles'])
-                
-                startDay = endDay
-                print(f'Day {i}: {len(results["articles"])}')
-        else:
-            numArticles = len(results['articles'])
-
-    return numArticles
+    return monthlyCount
         
 
 gdeltAPI = 'https://api.gdeltproject.org/api/v2/doc/doc'
