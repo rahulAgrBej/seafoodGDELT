908299d98fc1591b9c3435129f1889f78673e3c1
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-08 17:23:22 -0400

    granular search function adds to req queue

--- dataCollection/gdeltRequester.py
+++ dataCollection/gdeltRequester.py
@@ -48,6 +48,55 @@ COUNTRIES_LOCK.release()
 
 REQ_QUEUE = queue.Queue()
 
+# builds more granular searches and puts them on REQ_QUEUE
+def granularSearch(inList):
+
+    # collect start time
+    start = inList[3]
+    end = ''
+    searchRate = inList[6]
+    newSearchRate = ''
+
+    loopEnd = 0
+    loopStart = 0
+
+    if searchRate == 'monthly':
+        # make daily requests
+        month =  int(cdp.getMonth(start))
+        loopEnd = MONTH_END[month]
+        newSearchRate = 'daily'
+        
+    elif searchRate == 'daily':
+        # make hourly requests
+        loopEnd = 24
+        newSearchRate = 'hourly'
+
+    elif searchRate == 'hourly':
+        # make minute by minute requests
+        loopEnd = 60
+        newSearchRate = 'minute'
+    
+
+    for loopStart in range(loopEnd):
+        if searchRate == 'monthly':
+            end = cdp.incrementDay(start, 1)
+        elif searchRate == 'daily':
+            end = cdp.incrementHour(start, 1)
+        elif searchRate == 'hourly':
+            end = cdp.incrementMin(start, 1)
+        
+        # creating new list for more granular search
+        newReqList = copy.deepcopy(inList)
+        newReqList[3] = start
+        newReqList[4] = end
+        newReqList[6] = newSearchRate
+
+        REQ_QUEUE.put(newReqList)
+
+        start = end
+
+    return None
+
 # builds and sends the request from the variables in the REQ_QUEUE
 # if the MAX_RESULTS are found put more granular searches in the REQ_QUEUE
 def makeReq(inList):
@@ -60,6 +109,8 @@ def makeReq(inList):
     payload['STARTDATETIME'] = inList[3]
     payload['ENDDATETIME'] = inList[4]
     payload['QUERY'] = inList[5]
+    
+    searchRate = inList[6]
 
     # makes request
     resp = requests.get(GDELT_API, params=payload)
@@ -79,11 +130,11 @@ def makeReq(inList):
             # do something with articles results
         else:
             if searchRate == 'monthly':
-                granularSearch(inURL, inList, 'daily')
+                granularSearch(inList, 'daily')
             elif searchRate == 'daily':
-                granularSearch(inURL, inList, 'hourly')
+                granularSearch(inList, 'hourly')
             elif searchRate == 'hourly':
-                granularSearch(inURL, inList, 'minute')
+                granularSearch(inList, 'minute')
 
     return None
 
