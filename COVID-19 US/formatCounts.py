ea75209ef4cc35c7728d1498d3b73f9c6c0d508f
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-08-28 13:49:45 -0400

    all state data in R list format

---
+++ COVID-19 US/formatCounts.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+fileNames = os.listdir("statesInfo/")
+
+for fName in fileNames:
+    print(fName)
+    fpath = os.path.join("statesInfo/", fName)
+    f = open(fpath, 'r')
+    strFormat = f.read()
+    f.close()
+    if len(strFormat) > 0:
+        countData = json.loads(strFormat)
+    
+        countData = countData["results"][0]["timeline"]
+        wpath = os.path.join("statesDateCounts/", fName)
+        
+        wf = open(wpath, 'w')
+        wf.write(json.dumps(countData))
+        wf.close()
+
+print("done writing files")
