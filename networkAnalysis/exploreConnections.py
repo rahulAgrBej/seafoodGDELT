e10e88f52d4099d1ce6e42992f33687e56a8abf5
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-10-16 03:05:04 -0400

    collects all info including major countries

--- networkAnalysis/exploreConnections.py
+++ networkAnalysis/exploreConnections.py
@@ -3,6 +3,7 @@ import json
 import urllib.parse
 from itertools import permutations
 
+
 FREQ_API_URL = "https://article-search-api.herokuapp.com/api/searchTrends"
 
 countries = [
@@ -16,7 +17,15 @@ countries = [
     {"id": "US", "name": "United States", "descriptor": "American"}
     ]
 
+mainCountries = [
+    {"id": "US", "name": "United States"},
+    {"id": "CA", "name": "Canada"},
+    {"id": "CH", "name": "China"}
+    ]
+
 perms = permutations(countries, 2)
+
+csvFileText = "country1,country2,sourceCountry,frequency,norm,year,month,day"
 
 for entry in perms:
     sourceCountry = entry[0]
@@ -25,7 +34,19 @@ for entry in perms:
     query = "(" + targetCountry["name"] + " OR " + targetCountry["descriptor"] + ") (" + sourceCountry["name"] + " OR " + sourceCountry["descriptor"] + ") salmon (import OR imports OR export OR exports OR trade)"
     
     payload = {}
-    payload["countries"] = json.dumps([sourceCountry])
+
+    searchCountries = []
+    addCountry = True
+    for i in range(len(mainCountries)):
+        if sourceCountry["id"] != mainCountries[i]["id"]:
+            searchCountries.append(mainCountries[i])
+        else:
+            addCountry = False
+    
+    if addCountry:
+        searchCountries.append(sourceCountry)
+    
+    payload["countries"] = json.dumps(searchCountries)
     payload['q'] = json.dumps(query)
     payload['startDate'] = json.dumps("01/01/2017")
     payload['startTime'] = json.dumps("00:00:00")
@@ -40,53 +61,13 @@ for entry in perms:
     print(resp.status_code)
     data = resp.json()["results"]
     print(len(data))
+    #print(data)
+
     for source in data:
-        fName = targetCountry["name"] + " salmon trade source:" + source["query_details"]["title"][-2:]
-        fPath = "freqResults/" + fName
-        f = open(fPath, "w")
-        f.write(json.dumps(source["timeline"][0]["data"]))
-        f.close()
-
-
-"""
-for idx in range(len(countries)):
-
-    targetCountry = countries[idx]
-    sourceCountries = []
+        if len(source["timeline"]) > 0:
+            fName = targetCountry["id"] + sourceCountry["id"] + source["query_details"]["title"][-2:]+".txt"
+            fPath = "stage0/" + fName
+            f = open(fPath, "w")
+            f.write(json.dumps(source["timeline"][0]["data"]))
+            f.close()
     
-    if ((idx > 0) and (idx < (len(countries) - 1))):
-        sourceCountries = countries[0:idx]
-        sourceCountries.extend(countries[(idx + 1):])
-    elif idx == 0:
-        sourceCountries = countries[1:]
-    else:
-        sourceCountries = countries[:-1]
-
-    print(sourceCountries)
-    print()
-
-    query = "(" + targetCountry["name"] + " OR " + targetCountry["descriptor"] + ")  salmon (import OR imports OR export OR exports OR trade)"
-    
-    payload = {}
-    payload["countries"] = json.dumps(sourceCountries)
-    payload['q'] = json.dumps(query)
-    payload['startDate'] = json.dumps("01/01/2017")
-    payload['startTime'] = json.dumps("00:00:00")
-    payload['endDate'] = json.dumps("01/01/2018")
-    payload['endTime'] = json.dumps("00:00:00")
-
-    print(payload)
-
-    encodedPayload = urllib.parse.urlencode(payload)
-    finalURL = FREQ_API_URL + "?" + encodedPayload
-    resp = requests.get(finalURL)
-    print(resp.status_code)
-    data = resp.json()["results"]
-    print(len(data))
-    for source in data:
-        fName = targetCountry["name"] + " salmon trade source:" + source["query_details"]["title"][-2:]
-        fPath = "freqResults/" + fName
-        f = open(fPath, "w")
-        f.write(json.dumps(source["timeline"][0]["data"]))
-        f.close()
-"""
