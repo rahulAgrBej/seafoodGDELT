9a6d93f7fa13b88cfcbdea7218101bff9c468e4c
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-10-23 13:25:41 -0400

    re run R script with new TotalFullArticleData

--- networkAnalysis/R_working_dir/exploring_data.R
+++ networkAnalysis/R_working_dir/exploring_data.R
@@ -21,7 +21,8 @@ g <- ggplot(TotalFullArticleData %>% group_by(week) %>% tally(), aes(x = week,y 
   labs(y= "Number of Articles", x = "Week") +
   theme_minimal()
 
-jpeg(filename = "Complete_Summary.jpeg", height = 3, width = 4, units ="in", res = 400)
+jpeg(filename = file.path("figs", "Complete_Summary.jpeg"), 
+     height = 3, width = 4, units ="in", res = 400)
 print(g)
 dev.off()
 
@@ -34,7 +35,8 @@ g1 <- ggplot(US_CH %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
   labs(y= "Number of Articles", x = "Week") +
   theme_minimal()
 
-jpeg(filename = "US_CH.jpeg", height = 3, width = 4, units ="in", res = 400)
+jpeg(filename = file.path("figs", "US_CH.jpeg"), 
+     height = 3, width = 4, units ="in", res = 400)
 print(g1)
 dev.off()
 
@@ -46,7 +48,8 @@ g2 <- ggplot(CA_CH %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
   labs(y= "Number of Articles", x = "Week") +
   theme_minimal()
 
-jpeg(filename = "CA_CH.jpeg", height = 3, width = 4, units ="in", res = 400)
+jpeg(filename = file.path("figs", "CA_CH.jpeg"), 
+     height = 3, width = 4, units ="in", res = 400)
 print(g2)
 dev.off()
 
@@ -58,7 +61,8 @@ g2 <- ggplot(NO_CA %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
   labs(y= "Number of Articles", x = "Week") +
   theme_minimal()
 
-jpeg(filename = "NO_CA.jpeg", height = 3, width = 4, units ="in", res = 400)
+jpeg(filename = file.path("figs", "NO_CA.jpeg"), 
+     height = 3, width = 4, units ="in", res = 400)
 print(g2)
 dev.off()
 
@@ -70,6 +74,7 @@ g2 <- ggplot(NO_CI %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
   labs(y= "Number of Articles", x = "Week") +
   theme_minimal()
 
-jpeg(filename = "NO_CI.jpeg", height = 3, width = 4, units ="in", res = 400)
+jpeg(filename = file.path("figs", "NO_CI.jpeg"), 
+     height = 3, width = 4, units ="in", res = 400)
 print(g2)
 dev.off()
