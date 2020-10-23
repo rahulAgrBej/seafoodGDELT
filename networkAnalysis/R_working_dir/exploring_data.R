f9c58ebd2c3042db5112285af489a1df72cf5acf
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-10-23 12:09:12 -0400

    preliminary graphs and R script

---
+++ networkAnalysis/R_working_dir/exploring_data.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+
+TotalFullArticleData <- read_csv("TotalFullArticleData.csv")
+TotalFullArticleData <- TotalFullArticleData %>%
+  mutate(date = make_date(year, month, day)) %>%
+  mutate(week = strftime(date, format = "%V")) %>%
+  filter(!(str_detect(domain, "iheart.com$"))) # remove irrelevant sources with high number of hits
+
+TotalFullArticleData$week <- as.numeric(TotalFullArticleData$week)
+
+# plot everything on a weekly basis
+g <- ggplot(TotalFullArticleData %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
+  geom_line() +
+  labs(y= "Number of Articles", x = "Week") +
+  theme_minimal()
+
+jpeg(filename = "Complete_Summary.jpeg", height = 3, width = 4, units ="in", res = 400)
+print(g)
+dev.off()
+
+
+US_CH <- TotalFullArticleData %>%
+  filter((str_detect(country1, "US") & str_detect(country2, "CH")))
+
+g1 <- ggplot(US_CH %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
+  geom_line() +
+  labs(y= "Number of Articles", x = "Week") +
+  theme_minimal()
+
+jpeg(filename = "US_CH.jpeg", height = 3, width = 4, units ="in", res = 400)
+print(g1)
+dev.off()
+
+NO_SW
