70e73b308b74c5e903ef53621647b45dc57006e7
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-11-12 09:46:00 -0500

    writing raw data to file to process into a table later

--- allCountries/getCounts.py
+++ allCountries/getCounts.py
@@ -1,3 +1,5 @@
+import os
+import json
 import setup
 
 startDate = '01/01/2017'
@@ -8,7 +10,7 @@ endTime = '00:00:00'
 
 query = '(seafood OR fish OR crab OR shrimp)'
 
-combos = list(setup.allCombos())[:2]
+combos = list(setup.allCombos())[:1000]
 print(len(combos))
 
 reqs = setup.buildArticleCountsReqs(query, combos, startDate, startTime, endDate, endTime)
@@ -18,3 +20,9 @@ reqLimit = 15
 data = setup.sendCountReqs(reqs, reqLimit)
 print("THIS IS THE DATA")
 print(data)
+
+outFolder = 'tmpDataStorage/'
+outF = '0_10.txt'
+f = open(os.path.join(outFolder, outF), 'w')
+f.write(json.dumps(data))
+f.close()
