509c7d79569c7ced43736d09305c8caa166873ef
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-11-14 15:54:58 -0500

    example of how getCounts writing files every X combinations

--- allCountries/getCounts.py
+++ allCountries/getCounts.py
@@ -10,19 +10,18 @@ endTime = '00:00:00'
 
 query = '(seafood OR fish OR crab OR shrimp)'
 
-combos = list(setup.allCombos())[:1000]
-print(len(combos))
+combos = list(setup.allCombos())
 
-reqs = setup.buildArticleCountsReqs(query, combos, startDate, startTime, endDate, endTime)
-print(reqs)
-
-reqLimit = 15
-data = setup.sendCountReqs(reqs, reqLimit)
-print("THIS IS THE DATA")
-print(data)
-
-outFolder = 'tmpDataStorage/'
-outF = '0_1000.txt'
-f = open(os.path.join(outFolder, outF), 'w')
-f.write(json.dumps(data))
-f.close()
+for comboIdx in range(len(combos)):
+    
+    if (comboIdx > 0) and (comboIdx % 10 == 0):
+        reqs = setup.buildArticleCountsReqs(query, combos[comboIdx - 10:comboIdx], startDate, startTime, endDate, endTime)
+        reqLimit = 15
+        data = setup.sendCountReqs(reqs, reqLimit)
+        print("THIS IS THE DATA")
+        print(data)
+        outFolder = 'tmpDataStorage/'
+        outF = f'{str(comboIdx-10)}_{str(comboIdx)}.txt'
+        f = open(os.path.join(outFolder, outF), 'w')
+        f.write(json.dumps(data))
+        f.close()
