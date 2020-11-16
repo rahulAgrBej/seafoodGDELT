a9f157c528bf867179bb92ed8f90e5cd12cf6375
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-11-16 12:27:00 -0500

    changed to include reqs even if they arent % 1000

--- allCountries/getCounts.py
+++ allCountries/getCounts.py
@@ -10,16 +10,32 @@ endTime = '00:00:00'
 
 query = '(seafood OR fish OR crab OR shrimp)'
 
-combos = list(setup.allCombos())
+combos = list(setup.allCombos())[26000:]
+
+sendCombos = []
 
 for comboIdx in range(len(combos)):
-    
-    if (comboIdx > 0) and (comboIdx % 1000 == 0):
-        reqs = setup.buildArticleCountsReqs(query, combos[comboIdx - 1000:comboIdx], startDate, startTime, endDate, endTime)
+
+    sendCombos.append(combos[comboIdx])
+
+    if (len(sendCombos) % 1000 == 0):
+        reqs = setup.buildArticleCountsReqs(query, sendCombos, startDate, startTime, endDate, endTime)
         reqLimit = 15
         data = setup.sendCountReqs(reqs, reqLimit)
         outFolder = 'tmpDataStorage/'
         outF = f'{str(comboIdx-1000)}_{str(comboIdx)}.txt'
+        f = open(os.path.join(outFolder, outF), 'w')
+        f.write(json.dumps(data))
+        f.close()
+        print(f'{comboIdx} combinations searched for')
+        sendCombos = []
+
+if (len(sendCombos) > 0):
+    reqs = setup.buildArticleCountsReqs(query, sendCombos, startDate, startTime, endDate, endTime)
+        reqLimit = 15
+        data = setup.sendCountReqs(reqs, reqLimit)
+        outFolder = 'tmpDataStorage/'
+        outF = f'{str(len(combos)-1000)}_{str(len(combos))}.txt'
         f = open(os.path.join(outFolder, outF), 'w')
         f.write(json.dumps(data))
         f.close()
