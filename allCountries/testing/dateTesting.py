267641ddf3dc04b798062cbd36a88b1dae75d280
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-11-19 22:36:20 -0500

    finished date edge case testing it is end inclusive

--- allCountries/testing/dateTesting.py
+++ allCountries/testing/dateTesting.py
@@ -2,6 +2,12 @@ import requests
 import json
 import time
 
+def sumResults(data):
+    count = 0
+    for d in data:
+        count += d['value']
+    return count
+
 GDELT_URL = 'https://api.gdeltproject.org/api/v2/doc/doc'
 MAX_ARTICLES = 250
 
@@ -22,8 +28,24 @@ resp1 = requests.get(GDELT_URL, params=payload1)
 print(resp1.text)
 data1 = resp1.json()['timeline'][0]['data']
 
-count = 0
-for d in data1:
-    count += d['value']
+count1 = sumResults(data1)
 
-print(count)
+print(count1)
+
+# to satisfy GDELT requirements
+time.sleep(5)
+
+payload2 = {}
+payload2['QUERY'] = query1
+payload2['MODE'] = 'TimelineVolRaw'
+payload2['FORMAT'] = 'JSON'
+payload2['MAXRECORDS'] = MAX_ARTICLES
+payload2['STARTDATETIME'] = '20170101000000'
+payload2['ENDDATETIME'] = '20170101235959'
+
+resp2 = requests.get(GDELT_URL, params=payload2)
+print(resp2.text)
+data2 = resp2.json()['timeline'][0]['data']
+
+count2 = sumResults(data2)
+print(count2)
