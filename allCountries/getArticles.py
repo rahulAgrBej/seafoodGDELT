cfcfb2937493cb22c93b9f447a406255f460f60c
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-11-27 17:44:15 -0500

    article retrieval and recording in a CSV

--- allCountries/getArticles.py
+++ allCountries/getArticles.py
@@ -2,6 +2,11 @@ import os
 import json
 import requests
 import pprint
+import urllib.parse
+import setup
+
+
+ARTICLE_SEARCH_API = "https://article-search-api.herokuapp.com/api/getFullInfo"
 
 GDELT_REQ_LIMIT = 250
 
@@ -35,8 +40,80 @@ def makeReq(query, startDate, endDate):
 
     return req
 
-tmpDataFolder = 'tmpDataStorage/'
-dataFileNames = os.listdir(tmpDataFolder)[:2]
+def sendBatch(batch):
+
+    payload = {}
+    payload['requestsSent'] = json.dumps(batch)
+    resp = requests.get(ARTICLE_SEARCH_API + "?" + urllib.parse.urlencode(payload))
+
+    if resp.status_code == 200:
+        print('STATUS CODE 200')
+        responseData = resp.json()['results']
+    else:
+        responseData = []
+        print(resp.text)
+        print("ERROR OCCURED RESP")
+    
+    return responseData
+
+COUNTRIES = setup.country2Id()
+
+def findChar(text, character):
+    indices = []
+
+    for i in range(len(text)):
+        if text[i] == character:
+            indices.append(i)
+
+    return indices
+
+def processResponse(data):
+
+    for result in data:
+        query = result['query_details']['title']
+        sourceCountryCode = query[-2:]
+        
+        # finds quotation marks
+        indices = findChar(query, '\"')
+
+        if len(indices) != 4:
+            print("ERROR OCCURED FINDING QUOTATIONS")
+        
+        startQuote = indices[0] + 1
+        endQuote = indices[1]
+        country1 = query[startQuote:endQuote]
+        
+        startQuote = indices[2] + 1
+        endQuote = indices[3]
+        country2 = query[startQuote:endQuote]
+
+        firstCountryCode = COUNTRIES[country1]
+        secondCountryCode = COUNTRIES[country2]
+
+        csvContent = ''
+
+        if 'articles' in result:
+            for articleHit in result['articles']:
+                # replace all commas in titles
+                articleHit['title'].replace(',', '', )
+                csvContent = csvContent + \
+                    firstCountryCode + ',' + \
+                    secondCountryCode + ',' + \
+                    sourceCountryCode + ',' + \
+                    articleHit['seendate'][:4] + ',' + \
+                    str(int(articleHit['seendate'][4:6])) + ',' + \
+                    str(int(articleHit['seendate'][6:8])) + ',' + \
+                    articleHit['domain'] + ',' + \
+                    articleHit['title'].replace(',', '') + ',' + \
+                    articleHit['url'] + ',' + \
+                    articleHit['socialimage'] + ',' +\
+                    articleHit['language'] + ',' +\
+                    query[:-17] + '\n'
+    
+    return csvContent
+
+tmpDataFolder = 'tmpDataStorage2017/'
+dataFileNames = os.listdir(tmpDataFolder)
 
 completeFreqData = []
 
@@ -87,7 +164,7 @@ for entry in completeFreqData:
 
                 elif (count + date['value']) > GDELT_REQ_LIMIT:
                     # make new request
-                    endDate = prevDate[:-4] + '235959'
+                    endDate = prevDate[:-6] + '235959'
                     count = 0
                     reqs.append(makeReq(query, startDate, endDate))
 
@@ -104,15 +181,39 @@ for entry in completeFreqData:
             endDate = getCurrDate(entry['timeline'][0]['data'][-1])
             reqs.append(makeReq(query, startDate, endDate))
             count = 0
+        
+        startDate = ''
+        prevDate = ''
     
+#printer = pprint.PrettyPrinter()
+#print(len(reqs))
+#printer.pprint(reqs)
+
+API_REQ_LIMIT = 10
+
+batch = []
+
+csvContent = 'country1,country2,sourceCountry,year,month,day,domain,title,url,social_image,language,query\n'
+
+for req in reqs:
+
+    batch.append(req)
+
+    if len(batch) == API_REQ_LIMIT:
+        # send batch over to API
+        responseData = sendBatch(batch)
+        if len(responseData) > 0:
+            csvContent += processResponse(responseData)
+        batch = []
 
 
-printer = pprint.PrettyPrinter()
-print(len(reqs))
-printer.pprint(reqs)
+# edge case if there are any reqs left to be sent
+if len(batch) > 0:
+    # send batch
+    leftoverResponseData = sendBatch(batch)
+    if len(leftoverResponseData) > 0:
+        csvContent += processResponse(leftoverResponseData)
 
-API_REQ_LIMIT = 1
-
-# request API requesters
-
-# write to CSV
+f = open('articleResults/2017_part0.csv', 'w')
+f.write(csvContent)
+f.close()
