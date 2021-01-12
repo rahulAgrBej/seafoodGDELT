ca89950ea7407103c8c31430c2664cf7bfde242e
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-01-12 16:47:37 -0500

    tallying results be week however still need to fill weeks with zero articles in manually

--- allCountries/analysis_visualization/explore.R
+++ allCountries/analysis_visualization/explore.R
@@ -2,19 +2,24 @@ library(tidyverse)
 library(ggplot2)
 library(lubridate)
 
-articles <- read_csv("summaryTable.csv")
+articles <- read_csv("../summary_US_CH.csv")
 articles <- articles %>%
   mutate(date = make_date(year, month, day)) %>%
   mutate(week = strftime(date, format = "%V"))
 
 articles$week <- as.numeric(articles$week)
 
-g <- ggplot(articles %>% group_by(week) %>% summarise(freq = sum(freq)), aes(x = week,y = n)) +
+grouped_by_week <- articles %>% group_by(week) %>% tally()
+grouped_by_week
+
+
+g <- ggplot(articles %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
   geom_line() +
   labs(y= "Number of Articles", x = "Week") +
   theme_minimal()
 
-jpeg(filename = file.path("figs", "summary.jpeg"), 
+jpeg(filename = file.path("figs", "Summary_US_CH_2017.jpeg"), 
      height = 3, width = 4, units ="in", res = 400)
 print(g)
 dev.off()
+
