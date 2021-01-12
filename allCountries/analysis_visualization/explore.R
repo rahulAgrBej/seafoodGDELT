ef70267363944c7e1407ec093a9546eac9d17e3e
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-01-12 17:45:31 -0500

    now results are in a table with zeros filled in for weeks with no articles

--- allCountries/analysis_visualization/explore.R
+++ allCountries/analysis_visualization/explore.R
@@ -12,6 +12,17 @@ articles$week <- as.numeric(articles$week)
 grouped_by_week <- articles %>% group_by(week) %>% tally()
 grouped_by_week
 
+zerosFilledIn <- data.frame()
+zerosFilledIn.week <- seq.int(1, 52)
+zerosFilledIn.n <- rep(0, 52)
+zerosFilledIn.n[4] <- 50
+
+for (row in 1:nrow(grouped_by_week)) {
+  
+  currWeek <- as.integer(grouped_by_week[row, "week"])
+  freq <- as.integer(grouped_by_week[row, "n"])
+  zerosFilledIn.n[currWeek] <- freq
+}
 
 g <- ggplot(articles %>% group_by(week) %>% tally(), aes(x = week,y = n)) +
   geom_line() +
