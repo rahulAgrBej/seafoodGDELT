7c95596588979fc975ed7909512f43f3795e73df
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-01-13 14:00:44 -0500

    turned R script into functions to use for multiple years

--- allCountries/analysis_visualization/explore.R
+++ allCountries/analysis_visualization/explore.R
@@ -2,30 +2,6 @@ library(tidyverse)
 library(ggplot2)
 library(lubridate)
 
-articles <- read_csv("../summary_US_CH.csv")
-articles <- articles %>%
-  mutate(date = make_date(year, month, day)) %>%
-  mutate(week = strftime(date, format = "%V"))
-
-articles$week <- as.numeric(articles$week)
-
-grouped_by_week <- articles %>% group_by(week) %>% tally()
-grouped_by_week
-
-zerosFilledIn <- data.frame()
-zerosFilledIn.week <- seq.int(1, 52)
-zerosFilledIn.n <- rep(0, 52)
-zerosFilledIn.n[4] <- 50
-
-for (row in 1:nrow(grouped_by_week)) {
-  
-  currWeek <- as.integer(grouped_by_week[row, "week"])
-  freq <- as.integer(grouped_by_week[row, "n"])
-  zerosFilledIn.n[currWeek] <- freq
-}
-
-dataTS <- ts(zerosFilledIn.n)
-
 shock.id <- function(dat, thresh=0.35){
   # dat is your time series data and threshold is the threshold you want for Cook's D (defaulted to 0.35)
   outt <- array(dim=c(length(dat), 3))
@@ -44,7 +20,54 @@ shock.id <- function(dat, thresh=0.35){
   return(outt)
 }
 
-shocks <- shock.id(dataTS)
+
+articles.getTable <- function(filePath) {
+  articles <- read_csv(filePath)
+  articles <- articles %>%
+    mutate(date = make_date(year, month, day)) %>%
+    mutate(week = strftime(date, format = "%V")) %>%
+    filter(
+      !(str_detect(domain, "iheart.com$")) &
+        !(str_detect(domain, "floridastar.com$")) &
+        !(str_detect(domain, "jewishtimes.com$")) &
+        !(str_detect(domain, "oyetimes.com$"))) # remove irrelevant sources with high number of hits
+  
+  articles$week <- as.numeric(articles$week)
+  
+  return(articles)
+}
+
+articles.getTimeSeries <- function(articleTable) {
+  grouped_by_week <- articleTable %>% group_by(week) %>% tally()
+  
+  zerosFilledIn <- data.frame()
+  zerosFilledIn.week <- seq.int(1, 52)
+  zerosFilledIn.n <- rep(0, 52)
+  zerosFilledIn.n[4] <- 50
+  
+  for (row in 1:nrow(grouped_by_week)) {
+    
+    currWeek <- as.integer(grouped_by_week[row, "week"])
+    freq <- as.integer(grouped_by_week[row, "n"])
+    zerosFilledIn.n[currWeek] <- freq
+  }
+  
+  dataTS <- ts(zerosFilledIn.n)
+  
+  return(dataTS)
+}
+
+articles2017 <- articles.getTable("../summary_US_CH.csv")
+articles2018 <- articles.getTable("../summary_US_CH_2018.csv")
+articles2019 <- articles.getTable("../summary_US_CH_2019.csv")
+
+ts2017 <- articles.getTimeSeries(articles2017)
+ts2018 <- articles.getTimeSeries(articles2018)
+ts2019 <- articles.getTimeSeries(articles2019)
+
+shocks2017 <- shock.id(ts2017)
+shocks2018 <- shock.id(ts2018)
+shocks2019 <- shock.id(ts2019)
 
 g <- ggplot(zerosFilledIn, aes(x = zerosFilledIn.week,y = zerosFilledIn.n)) +
   geom_line() +
