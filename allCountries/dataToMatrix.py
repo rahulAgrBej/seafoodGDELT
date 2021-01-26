29167e946df0fedceb534f1e276ecf2ac1b3e89d
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-01-26 17:26:27 -0500

    counter mechanism for the matrix

--- allCountries/dataToMatrix.py
+++ allCountries/dataToMatrix.py
@@ -1,3 +1,7 @@
+import pprint
+
+pp = pprint.PrettyPrinter(indent=3)
+
 
 # Read in all countries and provide them an index
 countriesFilePath = 'countryLookUp.csv'
@@ -26,3 +30,24 @@ for line in countries:
 # Build a n x n matrix where n = number of all countries being considered
 # all values inside the matrix should be 0 by default
 # matrix will be symmetrical
+
+dataFilePath = ''
+dataF = open(dataFilePath, 'r')
+dataCountries = dataF.readlines()
+dataCountries.close()
+
+dataCountries = dataCountries[1:] # eliminates header row
+for row in rows:
+    row = row.rstrip('\n')
+    cells = row.split(',')
+    
+    # gets which countries are involved in this row
+    countryA = cells[0]
+    countryB = cells[1]
+    idxA = countryIdx[countryA]
+    idxB = countryIdx[countryB]
+
+    # adds to counter for each part of the matrix b/c it is symmetrical
+    countryMtrx[idxA][idxB] += 1
+    countryMtrx[idxB][idxA] += 1
+
