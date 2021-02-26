5c91857e4bf581a82cf709a7f1f860bdb840fa5f
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-02-25 23:54:23 -0500

    working for single plot for residual vs counts

--- allCountries/analysis_visualization/residualsAnalysis.R
+++ allCountries/analysis_visualization/residualsAnalysis.R
@@ -23,31 +23,47 @@ getResiduals <- function(data) {
   return(data)
 }
 
-test <- function(country) {
+getInfo <- function(country) {
   
   exports <- records.getFullCountryExports(country$name)
   exports <- getResiduals(exports)
   exports <- addTypeCol(exports,'TYPE', 'EXPORTS')
-  
-  
+  exports$COUNT_PLOT <-exports$residual
+  exports$MONTH_IDX <- seq(1, nrow(exports))
   
   imports <- records.getFullCountryImports(country$name)
   imports <- getResiduals(imports)
   imports <- addTypeCol(imports, 'TYPE', 'IMPORTS')
-  
+  imports$COUNT_PLOT <-imports$residual
+  imports$MONTH_IDX <- seq(1, nrow(imports))
   
   news <- records.getFullCountryNewsCounts(country$code)
   news <- addTypeCol(news, 'CTY_NAME', country$name)
   news <- getResiduals(news)
   news <- addTypeCol(news, 'TYPE', 'NEWS')
+  news$COUNT_PLOT <- news$COUNTS
+  news$MONTH_IDX <- seq(1, nrow(news))
   
   complete <- rbind(exports, imports, news)
+  print(complete)
   
   return(complete)
 }
 
 relevantCountries <- read_csv('data/relevantCountries.csv')
-chile <- relevantCountries[1,]
-print(test(chile))
+country <- relevantCountries[1,]
+countryInfo <- getInfo(country)
+
+p <- countryInfo %>%
+  ggplot() +
+  ggtitle(country$name) +
+  geom_line(aes(x=MONTH_IDX, y=COUNT_PLOT)) +
+  scale_x_continuous(breaks=seq(1,nrow(countryInfo)/3,by=1)) +
+  facet_grid(rows=vars(TYPE), scales='free_y') 
+
+plot(p)
+
+print(nrow(countryInfo))
+
 
 # Get all residuals for exports
