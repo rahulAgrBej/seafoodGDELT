8a6e38ffba90bc6eae5be80ab475a41dc35de58a
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-05-08 19:46:51 -0400

    finds and records all residuals and shocks for imports and exports

---
+++ trade_and_news/analysis.R
@@ -0,0 +1,101 @@
+
+# Shock Detection Function
+shock.id <- function(dat, thresh=0.25){
+  # dat is your time series data and threshold is the threshold you want for Cook's D (defaulted to 0.35)
+  outt <- array(dim=c(length(dat), 3))
+  x <- 1:length(dat)
+  ll <- lowess(x, dat, f=(2/3)) # Fits lowess curve (can specify other options for how the curve is estimated and can change the span)
+  rr <- as.numeric(dat[order(x)]-ll$y) #residuals off lowess
+  rrp1 <- rr[2:length(rr)] # Residuals at time t
+  rrm1 <- rr[1:(length(rr)-1)] # Residuals at time t-1
+  ll2 <- lm(rrp1~rrm1) # Linear fit of the residuals
+  cd <- cooks.distance(ll2) # Calculate the Cook's D
+  outt[2:length(rr),1] <- as.numeric(cd) # Output the Cook's D
+  outt[,2] <- rr # Output the residuals
+  outt[2:length(rr),3] <- ifelse(as.numeric(cd) >= thresh,1,0) # Logical of whether point is a shock
+  outt <- as.data.frame(outt)
+  colnames(outt) <- c("cooks.d", "residual", "shock.event")
+  return(outt)
+}
+
+# finds shocks for trade data
+findTradeShocks <- function(tradeData, countryName) {
+  
+  countryData <- tradeData %>%
+    filter(str_detect(CTY_NAME, countryName)) %>%
+    arrange(YEAR, MONTH)
+  
+  if (nrow(countryData) > 0) {
+    # if records exist for this country
+    tradeShocks <- shock.id(countryData$TOTAL) %>%
+      mutate(cooks.d = replace_na(cooks.d, 0)) %>%
+      mutate(shock.event = replace_na(shock.event, 0))
+  }
+  else {
+    # edge case there are no records for this country
+    tradeShocks <- data.frame(
+      cooks.d=rep(0, 48),
+      residual=rep(0,48),
+      shock.event=rep(0,48)
+    )
+  }
+  
+  return(tradeShocks)
+}
+
+# Trade data file paths and retrieval (PROCESSED VERSION)
+importFilePath <- 'data/processed/original/imports.csv'
+exportFilePath <- 'data/processed/original/exports.csv'
+
+importData <- read_csv(importFilePath)
+exportData <- read_csv(exportFilePath)
+
+countryListPath <- 'data/relevantCountries.csv'
+countryList <- read_csv(countryListPath)
+
+importShocks <- data.frame(
+  CTY_NAME=character(),
+  cooks.d=numeric(),
+  residual=numeric(),
+  shock.event=numeric()
+)
+
+exportShocks <- data.frame(
+  CTY_NAME=character(),
+  cooks.d=numeric(),
+  residual=numeric(),
+  shock.event=numeric()
+)
+
+for (countryIdx in 1:nrow(countryList)) {
+  countryTradeName <- countryList[countryIdx, ]$name
+  countryNewsCode <- countryList[countryIdx, ]$code
+  
+  print(countryTradeName)
+  
+  # detecting and recording shocks in imports
+  countryImportShocks <- findTradeShocks(importData, countryTradeName) %>%
+    cbind(data.frame(
+      'CTY_NAME'=rep(countryTradeName, 48)
+    ))
+  
+  importShocks <- importShocks %>%
+    rbind(countryImportShocks)
+  
+  # detecting and recording shocks in exports
+  countryExportShocks <- findTradeShocks(exportData, countryTradeName) %>%
+    cbind(data.frame(
+      'CTY_NAME'=rep(countryTradeName, 48)
+    ))
+  
+  exportShocks <- exportShocks %>%
+    rbind(countryExportShocks)
+}
+
+# write all trade shock data to output files
+importShockPath <- 'data/processed/original/importShocks.csv'
+exportShockPath <- 'data/processed/original/exportShocks.csv'
+
+write_csv(importShocks, importShockPath)
+write_csv(exportShocks, exportShockPath)
+
